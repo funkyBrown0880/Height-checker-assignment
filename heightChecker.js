40bf01d066aaac554179e83bbaea71c6f3378f8b
olufunke1011 <owoseniolufunke1011@gmail.com>
2023-12-11 06:47:48 +0100

    first commit

---
+++ heightChecker.js
@@ -0,0 +1,9 @@
+function standardHeight(){
+    let height
+    if (height >= 6.5){
+        console.log("Welcome to the Nigerian Army")
+    }else{
+        console.log("You are not eligible.")
+    }
+}
+standardHeight(3)
